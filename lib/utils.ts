cfa417da57a125267dd1f7a4ce5dabaeaa8c0815
zidduhhere <abi.alif@acetvm.com>
2026-05-31 09:49:50 +0530

    feat: add shared utility functions (cn, range, shuffle)

---
+++ lib/utils.ts
@@ -0,0 +1,19 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function range(n: number): number[] {
+  return Array.from({ length: n }, (_, i) => i + 1);
+}
+
+export function shuffle<T>(arr: T[]): T[] {
+  const a = [...arr];
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
